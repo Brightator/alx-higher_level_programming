3351c222374eb29de90a88e2a5918285420e6d2e
Bright Ogo <106819653+Brightator@users.noreply.github.com>
2022-10-11 02:46:15 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,15 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+    except(ValueError, TypeError):
+        return False
+    else:
+        return True
+
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
         int(value)
         return True
     except(ValueError):
